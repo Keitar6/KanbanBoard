5d979d09e1da7a4421584be4b8ee473cf971f315
Mateusz <statkprivate@gmail.com>
2023-10-22 17:16:47 +0200

    Redux persist implemented

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import { StrictMode } from "react";
 import App from "./App";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { reduxStore } from "./store/store";
+import { persistedStore, store } from "./store/store";
 import { ThemeWrapper } from "./components/ThemeWrapper";
 import { BrowserRouter } from "react-router-dom";
 import ErrorBoundary from "./utils/errorBoundary";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -15,8 +16,10 @@ root.render(
     <ErrorBoundary>
       <ThemeWrapper>
         <BrowserRouter>
-          <Provider store={reduxStore}>
-            <App />
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistedStore}>
+              <App />
+            </PersistGate>
           </Provider>
         </BrowserRouter>
       </ThemeWrapper>
--- src/store/store.ts
+++ src/store/store.ts
@@ -8,6 +8,8 @@ import loggerMiddleware from "redux-logger";
 
 import { userReducer } from "./reducers/user_slice";
 import { errorReducer } from "./reducers/error_slice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // or another storage engine
 
 export const rootReducer = combineReducers({
   user: userReducer,
@@ -18,16 +20,28 @@ export const rootMiddleware = (
   getDefaultMiddleware: CurriedGetDefaultMiddleware
 ) => getDefaultMiddleware().concat(loggerMiddleware);
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export type RootState = ReturnType<typeof rootReducer>;
 
 const storeInit = (preloadedState?: PreloadedState<RootState>) =>
   configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware: rootMiddleware,
     preloadedState,
   });
+const store = storeInit();
 
-export const reduxStore = storeInit();
+const persistedStore = persistStore(store);
+
+export const reduxStore = persistedStore;
+
+export { persistedStore, store };
 
 export type AppStore = ReturnType<typeof storeInit>;
 export type AppDispatch = AppStore["dispatch"];
